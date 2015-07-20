d057120cbdb9997b8a42c4854e76fafbfacd1c18
Jesse Shawl <jesse@jshawl.com>
2015-07-20 06:42:19 -0400

    add feedback

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 var card = function(image, clas){
   this.image = image;
   this.class = clas;
+  // awesome use of a constructor function!
+  // Typically, people use uppercase letters
+  // to name a constructor function, but this works
+  // just as well.
 }
 //////////////////////setting the card images/////////////////////
 var cards = [
@@ -51,18 +55,22 @@ var game = function(){
     }
       return match;
   }
+  // clicking clickly on several cards will keep a third card present. 
+  // I'm not sure which code here is allowing that - do you?
     //setting up variables
-  var cardNameArray = Object.keys(cards);
+  var cardNameArray = Object.keys(cards); //awesome use of .keys!
   var numClicks = 0;
   var cardArray = [];
   var classClicked;
   var compareArray = [];
   var sleeve = "url('https://pbs.twimg.com/profile_images/588424740223627264/oshWYgF9.jpg')"
+  // remember to download external images so this link won't 404
 
   //populate card Array
   for(var i = 0; i < cardNameArray.length; i++){ //creates an array of objects
     cardArray.push(cards[cardNameArray[i]]) //how to access the card objects
     cardArray.push(cards[cardNameArray[i]]) //creates a double for every card
+    // why create a double for every card?
   }
 
   Shuffle(cardArray);
@@ -79,9 +87,11 @@ var game = function(){
     if($(event.target).attr("class") == "alien"){
       var audio = new Audio('sound/alien.m4a');
       audio.play();
+      // nice!!
     }
     getTwoCards(event)
     if(numClicks == 2){ //this forbids clicking on more than 2 cards at a time
+      // maybe, but something else is allowing a third click in here.
       if(cardsMatch(compareArray) === false){ //cards don't match
         //need to use compare array to target class
         console.log("nope");
@@ -94,7 +104,7 @@ var game = function(){
       }
       else if(cardsMatch(compareArray) === true){ //cards match
         //removes event listener from matched cards
-        $("." + compareArray[0]).off()
+        $("." + compareArray[0]).off() // nice!
         //resets the compare array so it can be used again
         compareArray = [];
       }
@@ -123,3 +133,9 @@ $(".heart").on("mouseover", function(){
     $("nav").hide()
   },1500)
 })
+
+// Overall
+// Excellent OOJS Solution!!! Your code comments are very useful and make
+// your code very readable. I would encourage you to think about how you can reduce
+// the amount of global variables in your code. One popular option is to wrap
+// all of your code in a self-invoking anonymous function.
